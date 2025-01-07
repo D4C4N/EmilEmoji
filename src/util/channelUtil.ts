e4d2eed352d66d6d8c3cd47d49c22e9b159e35c2
Diogo Gomes <diogo.gomes@outlook.de>
2025-01-07 21:46:43 +0100

    move emoji event handlers to a separate file and add announcement channel utility function

---
+++ src/util/channelUtil.ts
@@ -0,0 +1,22 @@
+import { Guild, TextChannel } from "discord.js";
+
+/**
+ * Searches for the announcement channel in the guild.
+ * Returns the channel if found, otherwise returns null.
+ * @param guild - The guild to search for the announcement channel in.
+ * @returns 
+ */
+export const getAnnouncementChannel = (guild: Guild) => {
+    const announceChannelName = process.env.ANNOUNCE_CHANNEL || "general";
+
+    const channel = guild.channels.cache.find(
+        (channel): channel is TextChannel => channel.isTextBased() && channel.name === announceChannelName
+    );
+
+    if (!channel) {
+        console.log(`Channel '${announceChannelName}' not found in guild: ${guild.name}`);
+        return null;
+    }
+
+    return channel;
+};
